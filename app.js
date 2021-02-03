46ec75d57a8b958a5f2fa396c4bb4160b53a582a
MetroidFanMan <emb1270@gmail.com>
2021-02-03 13:10:19 -0600

    Updated app to reflect correct port and added Procfile

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(express.static('public'))
 
-mongoose.connect('mongodb://localhost:27017/todolistDB', {
+mongoose.connect('mongodb+srv://admin-ethan:test123@cluster0.vpxdd.mongodb.net/todolistDB?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
@@ -133,6 +133,11 @@ app.post('/delete', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000.')
+let port = process.env.PORT
+if (port == null || port == '') {
+  port = 8000
+}
+
+app.listen(port, () => {
+  console.log('Server has started successfully.')
 })
